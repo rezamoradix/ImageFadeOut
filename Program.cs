c401ef663736ab64a0f8a66a7ff96674df7befc6
RezaMoradi <rezamoradix@outlook.com>
2021-04-11 16:12:30 +0430

    Add start point

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using McMaster.Extensions.CommandLineUtils;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Collections.Generic;
 
 namespace ImageFadeOut
 {
@@ -15,13 +16,42 @@ namespace ImageFadeOut
         [Required]
         public string InputImage { get; }
 
-        [Option("-o|--output", Description = "The output image")]
+        [Option("-s|--start", Description = "Fade out start point")]
+        public int Start { get; } = 0;
+
+        [Argument(0, Description = "The output image")]
         public string OutputImage { get; } = "output.png";
 
         private void OnExecute()
         {
             Image source = Bitmap.FromFile(InputImage);
             Bitmap image = new Bitmap(source);
+
+            var b = new Bitmap(image.Width, image.Height);
+
+            float opacity = 1;
+            float opStep = opacity / (image.Height - Start);
+
+            if (Start != 0)
+                using (var g = Graphics.FromImage(b))
+                    g.DrawImage(image, new Rectangle(0, 0, image.Width, Start), 0, 0, image.Width, Start, GraphicsUnit.Pixel);
+
+            for (int i = Start; i < image.Height; i++)
+            {
+                using (var g = Graphics.FromImage(b))
+                {
+                    ColorMatrix cm = new ColorMatrix();
+                    cm.Matrix33 = opacity;
+
+                    ImageAttributes ia = new ImageAttributes();
+                    ia.SetColorMatrix(cm, ColorMatrixFlag.Default, ColorAdjustType.Default);
+
+                    g.DrawImage(image, new Rectangle(0, i, image.Width, 1), 0, i, image.Width, 1, GraphicsUnit.Pixel, ia);
+                }
+                opacity -= opStep;
+            }
+
+            b.Save(OutputImage, ImageFormat.Png);
         }
     }
 }
