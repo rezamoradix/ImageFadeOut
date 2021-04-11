9a538b4114778cf546d21fa57846b4f10db45d1f
RezaMoradi <rezamoradix@outlook.com>
2021-04-11 14:23:41 +0430

    Init

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using McMaster.Extensions.CommandLineUtils;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace ImageFadeOut
+{
+    public class Program
+    {
+        public static void Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+
+        [Option("-i|--input", Description = "The input image")]
+        [FileExists]
+        [Required]
+        public string InputImage { get; }
+
+        [Option("-o|--output", Description = "The output image")]
+        public string OutputImage { get; } = "output.png";
+
+        private void OnExecute()
+        {
+            Image source = Bitmap.FromFile(InputImage);
+            Bitmap image = new Bitmap(source);
+        }
+    }
+}
